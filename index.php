6e55f49d08ce04162c5fb84c32a740884cc3d25b
alelouar <kergoals@gmail.com>
2017-12-14 14:16:33 +0100

    Add connection to database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-	echo 'hello moijv !';
+require_once 'connexion.php';
+
+$queryGame = $connexion->prepare('select * from game');
+
+$queryGame->execute();
+
+$games = $queryGame->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+var_dump($games);
+echo '</pre>';
+
